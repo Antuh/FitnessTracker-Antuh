8080666c03e900f9dc7af62863d01e1d24cecbe8
Antuh <antuhsergey648@gmail.com>
2023-04-06 21:09:35 +0700

    Initial ten commit

--- app/src/main/java/com/example/fitnesstracker/MainActivity4.kt
+++ app/src/main/java/com/example/fitnesstracker/MainActivity4.kt
@@ -3,12 +3,15 @@ package com.example.fitnesstracker
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.AnalogClock
-import android.widget.Button
-import android.widget.Chronometer
-import android.widget.ImageView
+import android.os.Handler
+import android.view.View
+import android.widget.*
+import java.util.*
 
 class MainActivity4 : AppCompatActivity() {
+    private var seconds = 0
+    private var running = false
+    private var wasRunning = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main4)
@@ -32,16 +35,70 @@ class MainActivity4 : AppCompatActivity() {
             startActivity(Intent(this@MainActivity4, MainActivity5::class.java))
             finish()
         }
-        var chronometer: Chronometer = findViewById((R.id.chronometer2))
-        var butstart: Button = findViewById(R.id.buttonstart)
-        var stopbut: Button = findViewById(R.id.buttonpause)
-        butstart.setOnClickListener{
-            chronometer.start()
-
+        if (savedInstanceState != null) {
+            seconds = savedInstanceState
+                .getInt("seconds")
+            running = savedInstanceState
+                .getBoolean("running")
+            wasRunning = savedInstanceState
+                .getBoolean("wasRunning")
         }
-        stopbut.setOnClickListener{
-            chronometer.stop()
+        runTimer()
+    }
+    public override fun onSaveInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onSaveInstanceState(savedInstanceState)
+        savedInstanceState
+            .putInt("seconds", seconds)
+        savedInstanceState
+            .putBoolean("running", running)
+        savedInstanceState
+            .putBoolean("wasRunning", wasRunning)
+    }
+    override fun onPause() {
+        super.onPause()
+        wasRunning = running
+        running = false
+    }
+    override fun onResume() {
+        super.onResume()
+        if (wasRunning) {
+            running = true
         }
     }
-
+    fun onClickStart(view: View?) {
+        running = true
+    }
+    fun onClickStop(view: View?) {
+        running = false
+    }
+    fun onClickReset(view: View?) {
+        running = false
+        seconds = 0
+    }
+    private fun runTimer() {
+        val timeView = findViewById<View>(
+            R.id.chronometer2
+        ) as TextView
+        val handler = Handler()
+        handler.post(object : Runnable {
+            override fun run() {
+                val hours = seconds / 3600
+                val minutes = seconds % 3600 / 60
+                val secs = seconds % 60
+                val time = String.format(
+                    Locale.getDefault(),
+                    "%d:%02d:%02d", hours,
+                    minutes, secs
+                )
+                timeView.text = time
+                if (running) {
+                    seconds++
+                }
+                handler.postDelayed(this, 1000)
+            }
+        })
+    }
 }
+
